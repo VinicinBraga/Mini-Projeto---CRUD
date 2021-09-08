34cda0183b2a500722cedfeeb8b20553c8b19170
VinicinBraga <vinicius@bragamatta.com.br>
2021-09-08 10:55:45 -0300

    Implementando iteraçao do Crud com o Layout

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
-const closeModal = () =>
+const closeModal = () => {
   document.getElementById("modal").classList.remove("active");
-
-const tempClient = {
-  nome: "Renata",
-  email: "renata@gmail.com.br",
-  celular: "31987654321",
-  cidade: "Belo Horizonte",
+  clearFields();
 };
 
 const getLocalStorage = () =>
@@ -39,6 +34,52 @@ const deleteClient = (index) => {
   dbClient.splice(index, 1);
   setLocalStorage(dbClient);
 };
+//Iteração com layout
+const clearFields = () => {
+  const fields = document.querySelectorAll(".modal-field");
+  fields.forEach((field) => (field.value = ""));
+};
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
+const saveClient = () => {
+  if (isValidFields()) {
+    const client = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      celular: document.getElementById("celular").value,
+      cidade: document.getElementById("cidade").value,
+    };
+    createClient(client);
+    updateTable();
+    closeModal();
+  }
+};
+const createRow = (client) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>${client.nome}</td>
+    <td>${client.email}</td>
+    <td>${client.celular}</td>
+    <td>${client.cidade}</td>
+    <td>
+            <button type="button" class="button lightblue">editar</button>
+            <button type="button" class="button red">excluir</button>
+    </td>
+  `;
+  document.querySelector("#tableClient>tbody").appendChild(newRow);
+};
+
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tableClient>tbody tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
+};
+const updateTable = () => {
+  const dbClient = readClient();
+  clearTable();
+  dbClient.forEach(createRow);
+};
+updateTable();
 
 // Events
 document
@@ -46,3 +87,5 @@ document
   .addEventListener("click", openModal);
 
 document.getElementById("modalClose").addEventListener("click", closeModal);
+
+document.getElementById("salvar").addEventListener("click", saveClient);
